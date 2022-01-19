3ffac614b544b6a2d45cce9b9191bf97dfff74aa
Daniel891116 <yaotinghuang89@gmail.com>
2022-01-20 03:12:57 +0800

    Media Client

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
-from re import T
+from re import S, T
 import socket
+from ssl import SSL_ERROR_INVALID_ERROR_CODE
+from struct import pack
 import threading
 import time
 from typing import Dict
@@ -12,14 +14,15 @@ class MediaClient():
     IP = "127.0.0.0"
     RTPport: int = None
     mediaServer: socket.socket
-    RTSP_SETUP = False
+    RTSP_STATUS = RTSPPacket.INVALID
     RTP_IP = None
     RTP_send_port = None
     RTP_recv_port = None
+    RTSP_Thread = None
     sendThread = None
     recvThread = None
     Cseq: int = 1
-    CLIENT_BUFFER = 1024
+    SERVER_BUFFER = 1024
     RTP_TIMEOUT = 5  # ms
     SERVER_TIMEOUT = 100  # ms
 
@@ -28,25 +31,59 @@ class MediaClient():
         self.RTPport = port  # Port for RTP connection
     
     def start(self):
-        print(f'Media client start at {self.IP}:{self.RTPport}')
+        print(f'Media client starts at {self.IP}:{self.RTPport}')
         self.mediaServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.mediaServer.bind((self.IP, self.RTPport))
+        self.mediaServer.connect((self.IP, self.RTPport))
 
     def Send_SETUP_request(self):
-        while True:
-            req = RTSPPacket(request_type="SETUP", cseq=self.Cseq).to_bytes()
+        if self.RTSP_STATUS == RTSPPacket.INVALID:
+            req = RTSPPacket(request_type = RTSPPacket.SETUP, cseq = self.Cseq).to_bytes()
             self.mediaServer.send(req)
-            while True:
-                message = self.mediaServer.recv(self.CLIENT_BUFFER)
+            while self.RTSP_STATUS != RTSPPacket.SETUP:
+                message = self.mediaServer.recv(self.SERVER_BUFFER)
                 packet = RTSPPacket.from_bytes(message)
                 if packet.request_type == RTSPPacket.SETUP:
-                    self.RTSP_SETUP = True
-                    self.RTP_send_port = packet.port_recvfrom
-                    self.RTP_recv_port = packet.port_sendto
+                    self.RTSP_STATUS = RTSPPacket.SETUP    # 1 : SETUP
+                    self.RTP_send_port = packet.dst_port
                     self.RTP_IP = packet.ip
-                    self.sendThread = threading.Thread(target = self.RTP_send, args = (self.RTP_IP, self.RTP_send_port, ))
-                    self.recvThread = threading.Thread(target = self.RTP_recv, args = (self.RTP_IP, self.RTP_recv_port, ))
+                    self.Cseq += 1
+                    self.recvThread = threading.Thread(target = self.RTP_recv, args = (self.RTP_IP, self.RTP_recv_port))
 
+    def Send_PLAY_request(self):
+        if self.RTSP_STATUS == RTSPPacket.SETUP or self.RTSP_STATUS == RTSPPacket.PAUSE:
+            req = RTPPacket(request_type = RTSPPacket.PLAY, cseq = self.Cseq).to_bytes()
+            self.mediaServer.send(req)
+            while self.RTSP_STATUS != RTSPPacket.PLAY:
+                message = self.mediaServer.recv(self.SERVER_BUFFER)
+                packet = RTSPPacket.from_bytes(message)
+                if packet.request_type == RTPPacket.PLAY:
+                    self.Cseq += 1
+                    self.RTSP_STATUS = RTSPPacket.PLAY
+                    self.sendThread = threading.Thread(target = self.RTP_send, args = (self.RTP_IP, self.RTP_send_port))
+
+    def Send_PAUSE_request(self):
+        if self.RTSP_STATUS == RTSPPacket.PLAY:
+            req = RTPPacket(request_type = RTSPPacket.PAUSE, cseq = self.Cseq).to_bytes()
+            self.mediaServer.send(req)
+            while self.RTSP_STATUS != RTSPPacket.PAUSE:
+                message = self.mediaServer.recv(self.SERVER_BUFFER)
+                packet = RTSPPacket.from_bytes(message)
+                if packet.request_type == RTPPacket.PAUSE:
+                    self.RTSP_STATUS = RTSPPacket.PAUSE
+                    self.Cseq += 1
+
+    def Send_TEARDOWN_request(self):
+        if self.RTSP_STATUS != RTSPPacket.TEARDOWN:
+            req = RTPPacket(request_type = RTSPPacket.TEARDOWN, cseq = self.Cseq).to_bytes()
+            self.mediaServer.send(req)
+            while self.RTSP_STATUS != RTSPPacket.TEARDOWN:
+                message = self.mediaServer.recv(self.SERVER_BUFFER)
+                packet = RTSPPacket.from_bytes(message)
+                if packet.request_type == RTSPPacket.TEARDOWN:
+                    self.RTSP_STATUS = RTSPPacket.INVALID
+                    self.RTP_send_port = None
+                    self.RTP_IP = None
+                    self.Cseq += 1
 
     def RTP_recv(self, send_ip, RTP_recv_port):
         print("receiving thread started")
@@ -54,9 +91,9 @@ class MediaClient():
         port = RTP_recv_port
         send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
         send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        send_socket.bind((ip, port))
+        send_socket.connect((ip, port))
         send_socket.settimeout(self.RTP_TIMEOUT / 1000.)
-        while True:
+        while self.RTSP_STATUS != RTSPPacket.INVALID and self.RTSP_STATUS != RTSPPacket.TEARDOWN:
             payload = {}
             packet = RTPPacket(
                 RTPPacket.TYPE.IMG,
@@ -72,10 +109,10 @@ class MediaClient():
         port = RTP_send_port
         send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
         send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        send_socket.bind((ip, port))
+        send_socket.connect((ip, port))
         send_socket.settimeout(self.RTP_TIMEOUT / 1000.)
         frame, _ = CameraStream.get_next_frame()
-        while True:
+        while self.RTSP_STATUS == RTSPPacket.PLAY:
             payload = {
                 "current_display": frame
             }
@@ -87,3 +124,4 @@ class MediaClient():
             ).get_packet()
             send_socket.sendto(packet, (ip, port))
         
+    
---
+++ client/utils/RTP_packet.py
@@ -0,0 +1,81 @@
+from email import header
+
+
+class RTPPacket:
+    HEADER_SIZE = 12
+    VERSION = 0b10
+    PADDING = 0b0
+    EXTENSION = 0b0
+    CONTRIBUTOR_COUNT = 0x3
+    MARKER = 0b0
+    SSRC = 0x00000000
+
+    class TYPE:
+        IMG = 0
+        SOUND = 1
+        BOTH = 2
+        NONE = 3
+
+    def __init__(
+        self,
+        payload_type: int = None,
+        sequence_num: int = None,
+        timestamp: int = None,
+        payload: bytes = None
+    ):
+        self.payload = payload
+        self.payload_type = payload_type
+        self.sequence_num = sequence_num
+        self.timestamp = timestamp
+
+        zeroth_byte = (self.VERSION << 6) | (self.PADDING << 5) | (self.EXTENSION << 4) | self.CONTRIBUTOR_COUNT
+        first_byte = (self.MARKER << 7) | self.payload_type
+        second_byte = self.sequence_num >> 8
+        third_byte = self.sequence_num & 0xFF
+        fourth_to_seventh_bytes = [
+            (self.timestamp >> shift) & 0xFF for shift in (24, 16, 8, 0)
+        ]
+        eigth_to_eleventh_bytes = [
+            (self.SSRC >> shift) & 0xFF for shift in (24, 16, 8, 0)
+        ]
+        
+        self.header = bytes((
+            zeroth_byte,
+            first_byte,
+            second_byte,
+            third_byte,
+            *fourth_to_seventh_bytes,
+            *eigth_to_eleventh_bytes
+        ))
+
+    @classmethod
+    def from_packet(cls, packet: bytes):
+        if len(packet) < cls.HEADER_SIZE:
+            raise RuntimeError("Packet length is shorter than RTP header size.")
+
+        header = packet[: cls.HEADER_SIZE]
+        payload = packet[cls.HEADER_SIZE :]
+
+        payload_type = header[1] & 0x7F
+        sequence_number = header[2] << 8 | header[3]
+        timestamp = 0
+        for i, b in enumerate(header[4:8]):
+            timestamp = timestamp | b << (3 - i) * 8
+
+        return cls(
+            payload_type,
+            sequence_number,
+            timestamp,
+            payload
+        )
+    
+    def get_packet(self) -> bytes:
+        return bytes((*self.header, *self.payload))
+
+    def print_header(self):
+        for i, by in enumerate(self.header[:8]):
+            s = ' '.join(f"{by:08b}")
+            print(s, end=' ' if i not in (3, 7) else '\n')
+
+    def get_payload(self):
+        return self.payload.decode("utf-8")
