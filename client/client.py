9719dd5576aff3cb6b26d2abd0e11e5a1689fbed
Daniel891116 <yaotinghuang89@gmail.com>
2022-01-19 23:26:26 +0800

    media client

--- client/client.py
+++ client/client.py
@@ -4,7 +4,6 @@ import threading
 import time
 from typing import Dict
 
-from .user import User
 from utils.RTSP_packet import RTSPPacket
 from utils.RTP_packet import RTPPacket
 from utils.camera_stream import CameraStream
@@ -42,36 +41,30 @@ class MediaClient():
                 packet = RTSPPacket.from_bytes(message)
                 if packet.request_type == RTSPPacket.SETUP:
                     self.RTSP_SETUP = True
-                    self.RTP_send_port = packet.dst_port
+                    self.RTP_send_port = packet.port_recvfrom
+                    self.RTP_recv_port = packet.port_sendto
                     self.RTP_IP = packet.ip
                     self.sendThread = threading.Thread(target = self.RTP_send, args = (self.RTP_IP, self.RTP_send_port, ))
                     self.recvThread = threading.Thread(target = self.RTP_recv, args = (self.RTP_IP, self.RTP_recv_port, ))
 
-    def RTP_recv(self, RTP_IP, TRP_recv_port):
-        print("recv thread started")
-        ip = RTP_IP
-        port = TRP_recv_port
-        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
-        recv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        recv_socket.bind((ip, port))
-        recv_socket.settimeout(self.RTP_TIMEOUT / 1000.)
 
+    def RTP_recv(self, send_ip, RTP_recv_port):
+        print("receiving thread started")
+        ip = send_ip
+        port = RTP_recv_port
+        send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP socket
+        send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        send_socket.bind((ip, port))
+        send_socket.settimeout(self.RTP_TIMEOUT / 1000.)
         while True:
-            recv = bytes()
-            while True:
-                try:
-                    data, addr = recv_socket.recvfrom(1024)
-                    if user_ip == addr[0] and user_port == addr[1]:
-                        recv += data
-                    else:
-                        break
-                    if recv.endswith(CameraStream.IMG_END):
-                        break
-                except socket.timeout:
-                    continue
-            payload = RTPPacket.from_packet(recv).get_payload()
-            self.users[user_url].current_display = payload["img"]
-            time.sleep(self.SERVER_TIMEOUT / 1000.)
+            payload = {}
+            packet = RTPPacket(
+                RTPPacket.TYPE.IMG,
+                int(time.time()),
+                int(time.time()),
+                bytes(payload)
+            ).get_packet()
+            send_socket.sendto(packet, (ip, port))
 
     def RTP_send(self, send_ip, RTP_send_port):
         print("sending thread started")
@@ -81,9 +74,11 @@ class MediaClient():
         send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         send_socket.bind((ip, port))
         send_socket.settimeout(self.RTP_TIMEOUT / 1000.)
-
+        frame, _ = CameraStream.get_next_frame()
         while True:
-            payload = {}
+            payload = {
+                "current_display": frame
+            }
             packet = RTPPacket(
                 RTPPacket.TYPE.IMG,
                 int(time.time()),
