f759907d612dd54cee9ad9750d6828e602cdddd7
傅譽的MacBook <b08901039@ntu.edu.tw>
2022-01-21 17:37:27 +0800

    changable filename

--- client/mainClient.py
+++ client/mainClient.py
@@ -4,8 +4,10 @@ from mediaClient import ClientWindow
 
 if __name__ == "__main__":
     import sys
+    if len(sys.argv == 2):
+        filename = sys.argv[2]
     app = QApplication([])
-    client = ClientWindow("127.0.0.1", 3000)
+    client = ClientWindow("127.0.0.1", 3000, filename)
     client.resize(1280, 720)
     client.setFixedSize(1280,720)
     client.show()
